3732ed33d91fbd8923fd301e59cc2b3224ee6259
huangrongyuan <1728153759@qq.com>
2017-10-29 14:13:12 +0800

    10.29

--- app/src/main/java/cn/edu/gdmec/android/moblieguard/m2theftguard/Setup4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/moblieguard/m2theftguard/Setup4Activity.java
@@ -3,6 +3,7 @@ package cn.edu.gdmec.android.moblieguard.m2theftguard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import cn.edu.gdmec.android.moblieguard.R;
 
